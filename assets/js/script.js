7e4514e1450924314664c7fa548b73016bba9df9
Markpm-code <marlonseglos@yahoo.com>
2022-08-09 22:22:26 +0000

    Add updates on style .css for media querries

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ nextButton.addEventListener('click', () => {
 })
 
 function closeSubmit() {
-    modal.style.display ="none";
+    modal.style.display = "none";
 }
 
 function startGame() {
@@ -56,42 +56,42 @@ function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
+
 function selectAnswer(e) {
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
-    if(selectedButton.classList.contains("correct")) {
+    if (selectedButton.classList.contains("correct")) {
         console.log("correct");
         incrementCorrectAnswer();
-        
+
     } else {
         console.log("wrong");
-        incrementWrongAnswer(); 
+        incrementWrongAnswer();
     }
-    
+
     console.log(selectedButton);
     setStatusClass(document.body, correct);
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+        nextButton.classList.remove('hide')
     } else {
         startButton.innerText = 'Restart'
         startButton.classList.remove('hide')
     }
 }
 
-function incrementCorrectAnswer (){
-    
+function incrementCorrectAnswer() {
+
     let score = parseInt(document.getElementById("right").innerText);
     document.getElementById("right").innerText = ++score;
 }
 
-function incrementWrongAnswer () {
+function incrementWrongAnswer() {
 
     let score = parseInt(document.getElementById("wrong").innerText);
     document.getElementById("wrong").innerText = ++score;
@@ -110,102 +110,197 @@ function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
-const questions =[
-    {
+const questions = [{
         question: 'Who is the third man in the ring?',
-        answers: [
-            { text: 'The Referee', correct: true },
-            { text: 'The Coach', correct: false },
-            { text: 'The Ring Announcer', correct: false },
+        answers: [{
+                text: 'The Referee',
+                correct: true
+            },
+            {
+                text: 'The Coach',
+                correct: false
+            },
+            {
+                text: 'The Ring Announcer',
+                correct: false
+            },
         ]
     },
     {
         question: "How many minutes per round in Men's Professional Boxing?",
-        answers: [
-            { text: '2 minutes', correct: false },
-            { text: '5 minutes', correct: false },
-            { text: '3 minutes', correct: true}
+        answers: [{
+                text: '2 minutes',
+                correct: false
+            },
+            {
+                text: '5 minutes',
+                correct: false
+            },
+            {
+                text: '3 minutes',
+                correct: true
+            }
         ]
     },
     {
         question: 'What does KO mean?',
-        answers: [
-            { text: 'Knock Out', correct: true },
-            { text: 'Knock Off', correct: false },
-            { text: 'Knock On', correct: false }
+        answers: [{
+                text: 'Knock Out',
+                correct: true
+            },
+            {
+                text: 'Knock Off',
+                correct: false
+            },
+            {
+                text: 'Knock On',
+                correct: false
+            }
         ]
     },
     /**additional questions */
     {
         question: 'What does WBC stand for?',
-        answers: [
-            { text: 'World Boxing Corporation', correct: false },
-            { text: 'World Best Champions', correct: false },
-            { text: 'World Boxing Council', correct: true }
+        answers: [{
+                text: 'World Boxing Corporation',
+                correct: false
+            },
+            {
+                text: 'World Best Champions',
+                correct: false
+            },
+            {
+                text: 'World Boxing Council',
+                correct: true
+            }
         ]
     },
     {
         question: 'How many pounds is a Bantamweight?',
-        answers: [
-            { text: '118 pounds', correct: true },
-            { text: '115 pounds', correct: false },
-            { text: '122 pounds', correct: false }
+        answers: [{
+                text: '118 pounds',
+                correct: true
+            },
+            {
+                text: '115 pounds',
+                correct: false
+            },
+            {
+                text: '122 pounds',
+                correct: false
+            }
         ]
     },
     {
         question: "How many weight classes are there in Men's boxing?",
-        answers: [
-            { text: '15 weight classes', correct: false },
-            { text: '17 weight classes', correct: true },
-            { text: '12 weight classes', correct: false }
+        answers: [{
+                text: '15 weight classes',
+                correct: false
+            },
+            {
+                text: '17 weight classes',
+                correct: true
+            },
+            {
+                text: '12 weight classes',
+                correct: false
+            }
         ]
     },
     {
         question: 'A boxer who holds World titles from all of the major sanctioning body is called?',
-        answers: [
-            { text: 'Undisputed Champion', correct: true },
-            { text: 'Unified Champion', correct: false },
-            { text: 'Universal Champion', correct: false }
+        answers: [{
+                text: 'Undisputed Champion',
+                correct: true
+            },
+            {
+                text: 'Unified Champion',
+                correct: false
+            },
+            {
+                text: 'Universal Champion',
+                correct: false
+            }
         ]
     },
     {
         question: 'What is the Nationality of Katie Taylor?',
-        answers: [
-            { text: 'Irish', correct: true },
-            { text: 'British', correct: false },
-            { text: 'American', correct: false }
+        answers: [{
+                text: 'Irish',
+                correct: true
+            },
+            {
+                text: 'British',
+                correct: false
+            },
+            {
+                text: 'American',
+                correct: false
+            }
         ]
     },
     {
         question: 'What is the name of the boxer that is known as the Gypsy king?',
-        answers: [
-            { text: 'Tyson Fury', correct: true },
-            { text: 'Mike Tyson', correct: false },
-            { text: 'Anthony Joshua', correct: false }
+        answers: [{
+                text: 'Tyson Fury',
+                correct: true
+            },
+            {
+                text: 'Mike Tyson',
+                correct: false
+            },
+            {
+                text: 'Anthony Joshua',
+                correct: false
+            }
         ]
     },
     {
         question: 'Who is the only boxer that became an eight weight division Champion of the World?',
-        answers: [
-            { text: 'Floyd Mayweather', correct: false },
-            { text: 'Manny Pacquaio', correct: true },
-            { text: 'Ricky Hatton', correct: false }
+        answers: [{
+                text: 'Floyd Mayweather',
+                correct: false
+            },
+            {
+                text: 'Manny Pacquaio',
+                correct: true
+            },
+            {
+                text: 'Ricky Hatton',
+                correct: false
+            }
         ]
     },
     {
         question: 'In what Olympics did Katie Taylor won her Gold medal?',
-        answers: [
-            { text: 'London Olympics', correct: true },
-            { text: 'Beijing Olympics', correct: false },
-            { text: 'Tokyo Olympics', correct: false }
+        answers: [{
+                text: 'London Olympics',
+                correct: true
+            },
+            {
+                text: 'Beijing Olympics',
+                correct: false
+            },
+            {
+                text: 'Tokyo Olympics',
+                correct: false
+            }
         ]
     },
     {
         question: 'In what round did Ricky Hatton was knockout by Manny Pacqauio?',
-        answers: [
-            { text: '2nd Round', correct: true },
-            { text: '6th Round', correct: false},
-            { text: '10th Round', correct: false }
+        answers: [{
+                text: '2nd Round',
+                correct: true
+            },
+            {
+                text: '6th Round',
+                correct: false
+            },
+            {
+                text: '10th Round',
+                correct: false
+            }
         ]
     },
 ]
